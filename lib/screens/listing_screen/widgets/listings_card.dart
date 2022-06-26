954088da99f96d270b6938d0b43875613f651d71
DevAbaLancer <devsshaikh@gmail.com>
2022-06-26 18:48:18 +0530

    test

--- lib/screens/listing_screen/widgets/listings_card.dart
+++ lib/screens/listing_screen/widgets/listings_card.dart
@@ -15,7 +15,7 @@ class ListingsCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
+            color: Colors.grey.withOpacity(0.3),
             spreadRadius: 5,
             blurRadius: 7,
             offset: const Offset(0, 3), // changes position of shadow
